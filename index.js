978e30e74c3c848565abf8620793646d3b9ff6f1
Dipak Kandel <dipak.itsme@gmail.com>
2022-08-21 00:36:51 +0545

    third commit

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const paragraphs = [
 
   "Open your eyes in sea water and it is difficult to see much more than a murky, bleary green colour. Sounds, too, are garbled and difficult to comprehend. Without specialised equipment humans would be lost in these deep sea habitats, so how do fish make it seem so easy? Much of this is due to a biological phenomenon known as electroreception – the ability to perceive and act upon electrical stimuli as part of the overall senses. This ability is only found in aquatic or amphibious species because water is an efficient conductor of electricity.",
 
-  "Turning away from the ledge, he started slowly down the mountain, deciding that he would, that very night, satisfy his curiosity about the man-house. In the meantime, he would go down into the canyon and get a cool drink, after which he would visit some berry patches just over the ridge, and explore among the foothills a bit before his nap-time, which always came just after the sun had walked past the middle of the sky. At that period of the day the sun’s warm rays seemed to cast a sleepy spell over the silent mountainside, so all of the animals, with one accord, had decided it should be the hour for their mid-day sleep.",
+  "Turning away from the ledge, he started slowly down the mountain, deciding that he would, that very night, satisfy his curiosity about the man-house. In the meantime, he would go down into the canyon and get a cool drink, which always came just after the sun had walked past the middle of the sky. At that period of the day the sun’s warm rays seemed to cast a sleepy spell over the silent mountainside, so all of the animals, with one accord, had decided it should be the hour for their mid-day sleep.",
 
-  "The groundwork for keeping creative ability in check begins at school. School, later university and then work, teach us to regulate our lives, imposing a continuous process of restrictions which is increasing exponentially with the advancement of technology. Is it surprising then that creative ability appears to be so rare? It is trapped in the prison that we have erected. Yet, even here in this hostile environment, the foundations for creativity are being laid; because setting off on the creative path is also partly about using rules and regulations. Such limitations are needed so that once they are learnt, they can be broken.",
+  "The groundwork for keeping creative ability in check begins at school. School, later university and then work, teach us to regulate our lives, imposing a continuous process of restrictions which is increasing exponentially with the advancement of technology. It is trapped in the prison that we have erected. Yet, even here in this hostile environment, the foundations for creativity are being laid; because setting off on the creative path is also partly about using rules and regulations. Such limitations are needed so that once they are learnt, they can be broken.",
 
-  "Lifting barriers into place also plays a major part in helping the mind to control ideas rather than letting them collide at random. Parameters act as containers for ideas and thus help the mind to fix on them. When the mind is thinking laterally and two ideas from different areas of the brain come or are brought together, they form a new idea, just like atoms floating around and then forming a molecule. Once the idea has been formed, it needs to be contained or it will fly away, so fleeting is its passage.",
+  "Lifting barriers into place also plays a major part in helping the mind to control ideas rather than letting them collide at random. Parameters act as containers for ideas and thus help the mind to fix on them. When the mind is thinking laterally and two ideas from different areas of the brain come or are brought together, they form a new idea, just like atoms floating around and then forming a molecule.",
 
   "It had been a simple realization that had changed Debra's life perspective. It was really so simple that she was embarrassed that she had lived the previous five years with the way she measured her worth. Now that she saw what she had been doing, she could see how sad it was. That made her all the more relieved she had made the change. The number of hearts her Instagram posts received wasn't any longer the indication of her own self-worth.",
 
@@ -21,23 +21,22 @@ const paragraphs = [
 
   "It's an unfortunate reality that we don't teach people how to make money (beyond getting a 9 to 5 job) as part of our education system. The truth is there are a lot of different, legitimate ways to make money. That doesn't mean they are easy and that you won't have to work hard to succeed, but it does mean that if you're willing to open your mind a bit you don't have to be stuck in an office from 9 to 5 for the next fifty years o your life.",
 
-  "One fascinating use of active electroreception – known as the Jamming Avoidance Response mechanism – has been observed between members of some species known as the weakly electric fish. When two such electric fish meet in the ocean using the same frequency, each fish will then shift the frequency of its discharge so that they are transmitting on different frequencies. Doing so prevents their electroreception faculties from becoming jammed. Long before citizens’ band radio users first had to yell “Get off my frequency!” at hapless novices cluttering the air waves, at least one species had found a way to peacefully and quickly resolve this type of dispute.",
+  "One fascinating use of active electroreception known as the Jamming Avoidance Response mechanism has been observed between members of some species known as the weakly electric fish. When two such electric fish meet in the ocean using the same frequency, each fish will then shift the frequency of its discharge so that they are transmitting on different frequencies. Doing so prevents their electroreception faculties from becoming jammed. Long before citizens band radio users first had to yell “Get off my frequency!” at hapless novices cluttering the air waves, at least one species had found a way to peacefully and quickly resolve this type of dispute.",
 
   "Artesian water is drawn from a confined aquifer, a deep underground cavity of porous rock that holds water and bears pressure from a confining layer above it. This water can be accessed if companies drill a vertical channel down into the confined aquifer. Due to the pressurised nature of this aquifer, water will often rise up from within it and form a flowing artesian well, which appears as an explosive fountain at the earth’s surface. However, this only occurs when the surface is lower than the natural water table. If the surface is not lower than the natural water table, it is still possible to draw artesian water by using an extraction pump",
 
   "There are feasible alternatives to the bloat, extravagance and wasteful spending that comes with a modern Olympic Games. One option is to designate a permanent host city that would be re-designed or built from scratch especially for the task. Another is to extend the duration of the Olympics so that it becomes a festival of several months. Local businesses would enjoy the extra spending and congestion would ease substantially as competitors and spectators come and go according to their specific interests. Neither the “Olympic City” nor the extended length options really get to the heart of the issue, however.",
 
-  "After the last weary spectators and competitors have returned home, however, host cities are often left awash in high debts and costly infrastructure maintenance. The staggering expenses involved in a successful Olympic bid are often assumed to be easily mitigated by tourist revenues and an increase in local employment, but more often than not host cities are short changed and their taxpayers for generations to come are left settling the debt."
-
+  "After the last weary spectators and competitors have returned home, however, host cities are often left awash in high debts and costly infrastructure maintenance. The staggering expenses involved in a successful Olympic bid are often assumed to be easily mitigated by tourist revenues and an increase in local employment, but more often than not host cities are short changed and their taxpayers for generations to come are left settling the debt.",
 ];
-const paragraphID = document.getElementById('paragraph');
-const typingAreaID = document.getElementById('inputArea');
-const containerID = document.getElementById('container');
-const timerID = document.getElementById('timer');
-const speedID = document.getElementById('wpmcount')
-const accuracyID = document.getElementById('accuracyCount');
-const mistakesID = document.getElementById('mistakesCount');
-const wpmValueID = document.getElementById('wpmValue');
+const paragraphID = document.getElementById("paragraph");
+const typingAreaID = document.getElementById("inputArea");
+const containerID = document.getElementById("container");
+const timerID = document.getElementById("timer");
+const speedID = document.getElementById("wpmcount");
+const accuracyID = document.getElementById("accuracyCount");
+const mistakesID = document.getElementById("mistakesCount");
+const wpmValueID = document.getElementById("wpmValue");
 let executed = false;
 
 let totalCharacters = 0;
@@ -49,20 +48,16 @@ let accuracy = 0;
 // let characterTyped = 0;
 
 function processCurrentText() {
-
-
-  const arrayInputValue = typingAreaID.value.split("")
+  const arrayInputValue = typingAreaID.value.split("");
 
   if (!executed) {
-    countdown()
+    countdown();
     executed = true;
   }
-//  characterTyped++;
- mistakes = 0
+  //  characterTyped++;
+  mistakes = 0;
 
-
-
-  const arrayPara = paragraphID.querySelectorAll('span')
+  const arrayPara = paragraphID.querySelectorAll("span");
   arrayPara.forEach((characterSpan, index) => {
     const enteredCharacter = arrayInputValue[index];
 
@@ -70,31 +65,24 @@ function processCurrentText() {
 
     if (enteredCharacter == null) {
       // console.log('null')
-      characterSpan.classList.remove('correct')
-      characterSpan.classList.remove('incorrect')
-    }
-    else if (enteredCharacter === characterSpan.innerText) {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.remove("incorrect");
+    } else if (enteredCharacter === characterSpan.innerText) {
       // correctCharacters = correctCharacters + 1
       // console.log('correct')
-      characterSpan.classList.add('correct')
-      characterSpan.classList.remove('incorrect')
+      characterSpan.classList.add("correct");
+      characterSpan.classList.remove("incorrect");
+    } else {
+      characterSpan.classList.add("incorrect");
+      characterSpan.classList.remove("correct");
+      mistakes++;
     }
-    else {
-      
-      characterSpan.classList.add('incorrect')
-      characterSpan.classList.remove('correct')
-      mistakes ++;
-    }
-
-  
-  })
-// if (arrayPara)
-// speedID.innerHTML = characterTyped;
-mistakesID.innerHTML = totalErrors + mistakes;
-
+  });
+  // if (arrayPara)
+  // speedID.innerHTML = characterTyped;
+  mistakesID.innerHTML = totalErrors + mistakes;
 }
 function newParagraph() {
-  
   mistakes = 0;
   accuracyID.innerHTML = accuracy;
 
@@ -107,52 +95,52 @@ function newParagraph() {
   timerID.innerHTML = 60;
   totalCharacters = 0;
   // let wpm = 0;
-  if (timer){
-    clearTimeout(timer)
-    seconds = 10;
+  if (timer) {
+    clearTimeout(timer);
+    seconds = 60;
   }
   // mistakes = 0;
-  let content = paragraphs[Math.floor(Math.random() * paragraphs.length)]
-  paragraphID.innerText = '';
+  let content = paragraphs[Math.floor(Math.random() * paragraphs.length)];
+  paragraphID.innerText = "";
 
-  content.split('').forEach(character => {
-    const characterSpan = document.createElement('span')
-    characterSpan.innerText = character
-    paragraphID.appendChild(characterSpan)
+  content.split("").forEach((character) => {
+    const characterSpan = document.createElement("span");
+    characterSpan.innerText = character;
+    paragraphID.appendChild(characterSpan);
   });
 
   var paragraphHeight = paragraphID.clientHeight;
   var containerHeight = paragraphHeight * 2.5;
-  paragraphHeight = paragraphHeight + 'px';
-  containerHeight = containerHeight + 'px';
-  console.log(paragraphHeight)
-  typingAreaID.setAttribute('style', `min-height:${paragraphHeight}`)
-  containerID.setAttribute('style', `min-height:${containerHeight}`)
+  paragraphHeight = paragraphHeight + "px";
+  containerHeight = containerHeight + "px";
+  console.log(paragraphHeight);
+  typingAreaID.setAttribute("style", `min-height:${paragraphHeight}`);
+  containerID.setAttribute("style", `min-height:${containerHeight}`);
 }
-newParagraph()
+newParagraph();
 
 function countdown() {
-  var seconds = 10;
+  var seconds = 60;
   function tick() {
     seconds--;
-    timerID.innerHTML =
-      (seconds < 10 ? "0" : "") + String(seconds);
+    timerID.innerHTML = (seconds < 10 ? "0" : "") + String(seconds);
     if (seconds > 0) {
-       timer = setTimeout(tick, 1000);
-
+      timer = setTimeout(tick, 1000);
     }
 
-    if (seconds == 0 ) {
+    if (seconds == 0) {
       typingAreaID.disabled = true;
       speedID.innerHTML = totalCharacters;
-      mistakesID.innerHTML = mistakes; 
+      mistakesID.innerHTML = mistakes;
       correctCharacters = totalCharacters - mistakes;
-      wpm = (correctCharacters/5)/1 ;
+      wpm = correctCharacters / 5 / 1;
       wpmValueID.innerHTML = wpm;
       timerID.innerHTML = 60;
 
-      errorRate = Math.floor(((totalCharacters-correctCharacters)/ totalCharacters)*100);
-      let accuracy = 100-errorRate;
+      errorRate = Math.floor(
+        ((totalCharacters - correctCharacters) / totalCharacters) * 100
+      );
+      let accuracy = 100 - errorRate;
       accuracyID.innerHTML = accuracy;
       // mistakes;
 
@@ -171,11 +159,3 @@ function countdown() {
 // }
 // correctCharacters = document.querySelectorAll('correct').length;
 // accuracyID.innerHTML = correctCharacters;
-
-
-
-
-
-
-
-
